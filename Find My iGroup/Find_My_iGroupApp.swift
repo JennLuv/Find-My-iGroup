8f1bee902d77dd9e29f558e1ca9376b1c24ae792
Jen <jenniferluvindi@gmail.com>
2024-04-11 16:56:48 +0700

    Initial Commit

---
+++ Find My iGroup/Find_My_iGroupApp.swift
@@ -0,0 +1,17 @@
+//
+//  Find_My_iGroupApp.swift
+//  Find My iGroup
+//
+//  Created by Jennifer Luvindi on 11/04/24.
+//
+
+import SwiftUI
+
+@main
+struct Find_My_iGroupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Find My iGroupUITests/Find_My_iGroupUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Find_My_iGroupUITestsLaunchTests.swift
+//  Find My iGroupUITests
+//
+//  Created by Jennifer Luvindi on 11/04/24.
+//
+
+import XCTest
+
+final class Find_My_iGroupUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
